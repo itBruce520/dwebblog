e65f5a9f100258f0002ab37c4dc08badc2c8cec0
itBruce <93927430@qq.com>
2022-05-06 23:53:13 +0800

    master

---
+++ dwebhtml/src/temp.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <!-- 头部导航 -->
+    <div id="top-menu" class="dweb" ></div>
+    <!-- 侧边栏 左 导航 -->
+    <div id="left-menu" class="dweb" >
+      <el-icon id="left-btn">
+        <Menu />
+      </el-icon>
+    </div>
+    <!-- 页面内容 -->
+    <div id="contenct">
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+        <el-col :span="8">
+          <div class="dweb" style="height: 250px"></div>
+        </el-col>
+      </el-row>
+      <div id="footer" class="dweb">
+        <span>Copyright @ 2022 Bruce 工作室</span>
+      </div>
+    </div>
+  </div>
+</template>
